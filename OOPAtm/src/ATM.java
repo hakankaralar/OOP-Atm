a0ecacf3d5be978a3b262c0484a9a9f3176e17ad
hakankaralar <hakankaralar27@gmail.com>
2023-05-13 00:19:48 +0300

    OOP Atm

---
+++ OOPAtm/src/ATM.java
@@ -0,0 +1,76 @@
+
+import java.util.Scanner;
+
+public class ATM {
+    public void calıs(Hesap hesap){
+        Login login = new Login(); 
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bankamıza hoşgeldiniz...");
+        System.out.println("****************");
+        System.out.println("Kullanıcı girişi: ");
+        System.out.println("****************");
+        
+        int giris_hakki= 3;
+        while(true){
+            if(login.login(hesap)){
+                System.out.println("Giriş başarılı.");
+                break;
+                
+            }
+            else{
+                System.out.println("Giriş başarısız.");
+                giris_hakki -= 1;
+                System.out.println("Kalan giriş hakkı:" + giris_hakki);
+                
+                
+            }
+            if(giris_hakki == 0){
+                System.out.println("Giriş hakkınız bitti.");
+                return;
+            }
+              
+        }
+        System.out.println("*************");
+        String islemler = "1. Bakiye Görüntüle\n"
+                        + "2. Para Yatırma\n"
+                        + "3. Para Çekme\n"
+                        + "Çıkış için q ya basın";
+        System.out.println(islemler);
+        System.out.println("*************");
+        
+        while(true){
+            
+            System.out.print("İşlemi Seçiniz...");
+            String islem = scanner.nextLine();
+            
+            if(islem.equals("q")){
+                break;
+            }
+            else if(islem.equals("1")) {
+                System.out.println("Bakiyeniz: "+ hesap.getBakiye());
+                
+            }
+            
+            else if (islem.equals("2")){
+                System.out.print("Yatırılacak tutarı girin.");
+                int tutar = scanner.nextInt();
+                scanner.nextLine();
+                hesap.paraYatir(tutar);
+                
+            }
+            else if(islem.equals("3")){
+                System.out.print("Çekilecek tutarı girin:");
+                int tutar = scanner.nextInt();
+                scanner.nextLine();
+                hesap.paraCek(tutar);
+                
+            }
+            else {
+                System.out.println("Geçersiz işlem...");
+            }
+        }
+          
+    }
+}
---
+++ OOPAtm/src/Main.java
@@ -0,0 +1,19 @@
+ 
+public class Main {
+    public static void main(String[] args) {
+     
+    ATM atm = new ATM();
+    
+    Hesap hesap = new Hesap("Hakan","12345",2000);
+    
+    atm.calıs(hesap); 
+    System.out.println("Programdan Çıkılıyor.");
+    
+    
+    
+    }
+    
+    
+    
+    
+}
